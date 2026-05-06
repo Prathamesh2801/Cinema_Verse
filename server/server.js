eb46fccfee0c90e31769ca76a957fc8156a9e164
Prathamesh2801 <truepratham2801@gmail.com>
2026-05-06 18:02:21 +0530

    Fix production port logging

--- server/server.js
+++ server/server.js
@@ -3,11 +3,9 @@ import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port : http://localhost:${PORT}`);
+  console.log(`Server running on port : ${PORT}`);
 });
-
-
